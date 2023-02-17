e102c517e1eb55d25fd89bcbe030a24f49b53e8f
marcel <marcelisong64@gmail.com>
2023-02-17 01:33:30 -0800

    6 size commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,10 @@ int main(void)
 	long int c;
 	long long d;
 	float f;
-
 	printf("Size of a char: %lu bytes(s)\n", (unsigned long)sizeof(a));
 	printf("Size of an int: %lu bytes(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
 	printf("Size of a float: %lu bytes(s)\n", (unsigned long)sizeof(f));
-	return (o);
+	return (0);
 }
