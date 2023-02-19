781633a693bc0e942983f244079725676d2e3747
marcel <marcelisong64@gmail.com>
2023-02-19 10:49:46 -0800

    print alphabet commits

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	char qNe;
+
+	for (qNe = 'a';qNe <= 'z'; qNe++)
+	{
+		if (qNe != 'e' && qNe != 'q')
+			putchar(qNe);
+	}
+	putchar('\n')|;
+	return (0)
+}
