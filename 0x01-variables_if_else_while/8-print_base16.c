8cb17e601c414dfcb7ada8adda0e98988a63798e
marcel <marcelisong64@gmail.com>
2023-02-19 12:00:50 -0800

    print base commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stddio.h>
+/**
+ * main - to print numbers in base 16 in lowercase
+ * Return: Always 0
+ */
+int main(void)
+{
+	int numbers;
+	char letters;
+
+	for (number = 0; number <10; number++)
+		putchar('0' + number);
+	for (letters = 'a'; letters <= 'f' letters++)
+		putchar(letters);
+	putchar('\n');
+	return (0);
+}
