4efb1095a464f11276d5a7eac4e59e09c5ee51d8
marcel <marcelisong64@gmail.com>
2023-02-18 17:54:29 -0800

    print alphabets c commits

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: ALWAYS 0
+ */
+
+int main(void)
+{
+	char lower_case;
+
+	for (lower_case = 'a'; lower_case <= 'z';lower_case++)
+
+		putchar('\n');
+
+	        return (0);
+}
