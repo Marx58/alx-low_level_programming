5a2e45c6220d5a2936b7d2b9541a408037aea8e3
marcel <marcelisong64@gmail.com>
2023-03-15 17:22:09 -0700

    updated

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - allocates enough memory
+ * @c: character variable
+ * @size: size to be assigneed
+ *
+ * Return: returns pointer
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *myPointer;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	myPointer = (char *) malloc(size * sizeof(char));
+
+	if (!myPointer)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		myPointer[i] = c;
+	}
+	return (myPointer);
+}
